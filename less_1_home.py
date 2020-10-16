a9d7f7c44ed160219a1a564b4fcec31b6dbe2d73
ZubkovskayaAlexandra <72622729+ZubkovskayaAlexandra@users.noreply.github.com>
2020-10-16 22:31:56 +0200

    Add files via upload

---
+++ less_1_home.py
@@ -0,0 +1,45 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:37:30) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, World!')
+Hello, World!
+>>> a = 'Маша'
+>>> b = 'Петя'
+>>> c = 'Любовь'
+>>> print(a,'+',b,'=',c)
+Маша + Петя = Любовь
+>>> x = 3+4
+>>> print(x)
+7
+>>> 
+>>> 
+>>> 
+>>> x = 'Hello, World!' - присвоение переменной текста
+SyntaxError: invalid syntax
+>>> #x = 'Hello, World!' - присвоение переменной текста
+>>> #x = 3 + 4 - присвоение переменной математического выражения
+>>> #x = 3 / 4 - присвоение переменной математического выражения
+>>> #x = [1, 2, 5, 10, 100] - рисвоение переменной списка
+>>> 
+>>> 
+>>> 
+>>> номер3
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    номер3
+NameError: name 'номер3' is not defined
+>>> #3
+>>> kar = [1,5 , 'Good', 'Bad']
+>>> may = [9, 'Blue', 'Red', 11]
+>>> print (kar + may) #результат сложения списков a и b
+[1, 5, 'Good', 'Bad', 9, 'Blue', 'Red', 11]
+>>> print (int(kar[1]) ** may[3]) # возводить слово в степень
+48828125
+>>> print (kar[1] ** may[3])
+48828125
+>>> print (kar[0] * may[0])
+9
+>>> print (kar[2] + may[2])
+GoodRed
+>>> print (kar[1] + may[3])
+16
+>>> 
