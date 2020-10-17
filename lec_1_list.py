aacb84c398b04ab2d695550a576d76e56cfd426e
ZubkovskayaAlexandra <72622729+ZubkovskayaAlexandra@users.noreply.github.com>
2020-10-17 13:36:16 +0200

    Commit

---
+++ lec_1_list.py
@@ -0,0 +1,28 @@
+a = [1, 3, 6]
+
+print(a[0])
+
+b = [8, 10, 11]
+
+c = a + b
+
+
+
+
+d = []
+
+d.append(a)
+
+print(d)
+
+d.append(b)
+
+print(d)
+
+print(d[0])
+
+print(d[0][0])
+
+d[0] = 'Hello'
+
+print(d)
---
+++ lec_1_string.py
@@ -0,0 +1,6 @@
+a = 'Good'
+b = 'Bad'
+print(a+b) #сложение строк
+
+print(a*3)
+
