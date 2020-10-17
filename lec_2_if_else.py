2964a8112dd1d11fbad637d54582242904c252b5
ZubkovskayaAlexandra <72622729+ZubkovskayaAlexandra@users.noreply.github.com>
2020-10-17 14:31:21 +0200

    молодец

---
+++ lec_2_if_else.py
@@ -0,0 +1,7 @@
+a = int(input('введите a :'))
+
+if a>4:
+    print('hello 4')
+else:
+    print('hello a')
+
---
+++ lec_2_log.oper.py
@@ -0,0 +1,22 @@
+a = int(input('введите a :'))
+
+b = int(input('введите b :'))
+
+c = int(input('введите c :'))
+
+if a > 3 and b ==2:
+    print('good')
+elif b>3 or c ==5:
+    print('best')
+else:
+    print('bad')
+    
+    a = 3
+    b = 5
+    c = -1
+    
+    
+    
+    
+    
+    
