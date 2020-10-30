4f44278871a805cf2463d89d6c890c3816068354
ZubkovskayaAlexandra <72622729+ZubkovskayaAlexandra@users.noreply.github.com>
2020-10-30 16:00:48 +0200

    молодэс

--- lab.py
+++ lab.py
@@ -1,1 +1,63 @@
-lknk
+
+print('Hello, World!')
+a = 'Маша'
+
+b = 'Петя'
+
+c = 'Любовь'
+
+print(a,'+',b,'=',c)
+
+x = 3 + 4
+
+print(x)
+
+
+#номер 2 
+type('Hello, World')
+
+print(type('Hello, World'))
+
+type(3+4)
+
+print(type(3+4))
+
+type(3/4)
+
+print(type(3/4))
+
+type( [1, 2, 5, 10, 100])
+
+print(type( [1, 2, 5, 10, 100]))
+
+
+#номер3 
+d = (3/2)
+
+e = (16.7*4.32)
+
+f = (31/12)
+
+xa = 3
+
+s = (xa ** 3.4)
+
+y = ((d + e) / 14.5 + f - s )
+
+print(y)
+
+
+#номер4
+k = [1, 5, 'Good', 'Bad']
+
+m = [9, 'Blue ', 'Red', 11]
+
+print(k[1] + m[3])
+
+print(k[2] + m[2])
+
+print(k[0] * m[0])
+
+print(k[1] ** m[3])
+
+print(k + m)
